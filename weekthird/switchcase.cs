26a58cd5f319be326314a5375a5a8339f6f2d686
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-06-13 12:50:32 +0530

    update

---
+++ weekthird/switchcase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekthird
+{
+    internal class switchcase
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Enter 1st no:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd no:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplication");
+            Console.WriteLine("4: Division");
+            Console.WriteLine("5: Module");
+            Console.WriteLine("Select your option:");
+            int op = Convert.ToInt32(Console.ReadLine());
+
+            switch(op)
+            {
+                case 1:
+                    Console.WriteLine("You have selected Addition");
+                   int sum = n1 + n2;
+                    Console.WriteLine($"sum is:{sum}");
+                    break;
+
+                case 2:
+                    Console.WriteLine("You have selected Subtraction");
+                    int sub = n1 - n2;
+                    Console.WriteLine($"Sub is:{sub}");
+                    break;
+
+                case 3:
+                    Console.WriteLine("You have selected Multiplication");
+                    int mul = n1 * n2;
+                    Console.WriteLine($"Mul is: {mul}");
+                    break;
+
+                case 4:
+                    Console.WriteLine("You have selected division");
+                    double div =(double) n1 / n2;
+                    Console.WriteLine($"Div is: {div}");
+                    break;
+
+                case 5:
+                    Console.WriteLine("You have selected module");
+                    int mod = n1 % n2;
+                    Console.WriteLine($"Mod is: {mod}");
+                    break;
+
+                default:
+                    Console.WriteLine(" Default option");
+
+                    break;
+
+
+
+            }
+        }
+    }
+}
---
+++ weekthird/switchcase2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekthird
+{
+    internal class switchcase2
+    {
+        static void Main(string[] args)
+        {
+            //int num;
+            //Console.WriteLine("Enter :");
+            //num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nChoose your number to print your lucky day:");
+            Console.WriteLine("0: Sunday");
+            Console.WriteLine("1: Monday");
+            Console.WriteLine("2: Tuesday");
+            Console.WriteLine("3: Wednesday");
+            Console.WriteLine("4: Thursday");
+            Console.WriteLine("5: Friday");
+            Console.WriteLine("6: Saturday");
+            Console.WriteLine("Enter your number:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            
+
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("Sunday  is your lucky day");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday is your lucky day");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday is your lucky day");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday is your lucky day");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday is your lucky day");
+                    break;  
+                case 5:
+                    Console.WriteLine("Friday is your lucky day");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday is your lucky day");
+                    break;
+                default:
+                    Console.WriteLine("ooh wrong number entered");
+                    break;
+            }
+        }
+    }
+}
